be87885d63a5d1ab0b3301fc322173c1cfc72f68
Kamil Czop <myknakryu@gmail.com>
2023-06-10 03:04:09 +0200

    failed attempt

--- oldMain.py
+++ oldMain.py
@@ -10,15 +10,12 @@ from statistics import median, mean
 from collections import Counter
 
 LR = 1e-3
-env = gym.make("LunarLander-v2", render_mode = None)
+env = gym.make("CartPole-v1", render_mode = None)
 
 env.reset()
 goal_steps = 500
-score_requirement = 100
+score_requirement = 50
 initial_games = 10000
-
-print("-- Observations",env.observation_space)
-print("-- actionspace",env.action_space)
 
 def initial_population():
     training_data = []
@@ -30,7 +27,7 @@ def initial_population():
             game_memory = []
             prev_observation = []
             for _ in range(goal_steps):
-                action = env.action_space.sample()
+                action = random.randrange(0,2)
                 observation, reward, done, truncated, info = env.step(action)
 
                 if len(prev_observation) > 0:
@@ -44,15 +41,11 @@ def initial_population():
             if score >= score_requirement:
                 accepted_scores.append(score)
                 for data in game_memory:
-                    match data[1]:
-                        case 0:
-                            output = [1,0,0,0]
-                        case 1:
-                            output = [0,1,0,0]
-                        case 2:
-                            output = [0,0,1,0]
-                        case 3:
-                            output = [0,0,0,1]
+                    if data[1] == 1:
+                        output = [0,1]
+
+                    elif data[1] == 0:
+                        output = [1,0]
 
                     training_data.append([data[0], output])
 
@@ -64,16 +57,10 @@ def initial_population():
     print('count: ', Counter(accepted_scores))
     return training_data
 
+initial_population()
 
-try:
-    training_data = np.load('initial.npy', allow_pickle=True)
-except IOError:
-    training_data = initial_population()
-    save_data = np.array(training_data)
-    np.save('initial.npy', save_data)
-
-def neural_network_model():
-    network = input_data(shape=[None, env.observation_space.shape[0]], name='input')
+def neural_network_model(input_size):
+    network = input_data(shape=[None, input_size, 1], name='input')
 
     network = fully_connected(network, 128, activation='relu')
     network = dropout(network, 0.8)
@@ -90,30 +77,31 @@ def neural_network_model():
     network = fully_connected(network, 128, activation='relu')
     network = dropout(network, 0.8)
 
-    network = fully_connected(network, env.action_space.n, activation='softmax')
-    network = regression(network, optimizer='adam', loss = 'categorical_crossentropy', name = 'targets')
+    network = fully_connected(network, 2, activation='softmax')
+    network = regression(network, optimizer='adam', learning_rate = LR, loss = 'categorical_crossentropy', name = 'targets')
     
-    model = tflearn.DNN(network)
+    model = tflearn.DNN(network, tensorboard_dir = 'log')
 
     return model
 
 def train_model(training_data, model=False):
-    x = np.array([i[0] for i in training_data]).reshape(-1, len(training_data[0][0]))
+    x = np.array([i[0] for i in training_data]).reshape(-1,len(training_data[0][0]), 1)
     y = [i[1] for i in training_data]
 
     if not model:
-        model = neural_network_model()
+        model = neural_network_model(input_size=len(x[0]))
 
-    model.fit({'input':x}, {'targets':y}, n_epoch=10, snapshot_step=5000, show_metric=True, run_id='tak')
+    model.fit({'input':x}, {'targets':y}, n_epoch=10, snapshot_step=500, show_metric=True, run_id='tak')
     
     return model
 
+training_data = initial_population()
 model = train_model(training_data)
 
 
 scores = []
 choices = []
-env = gym.make("LunarLander-v2", render_mode = 'human')
+env = gym.make("CartPole-v1", render_mode = 'human')
 for each_game in range(10):
     score = 0;
     game_memory = []
@@ -122,9 +110,9 @@ for each_game in range(10):
     for _ in range(goal_steps):
         env.render()
         if len(prev_obs) == 0:
-            action = random.randrange(0,4)
+            action = random.randrange(0,2)
         else:
-            action = np.argmax(model.predict(prev_obs.reshape(-1, len(prev_obs)))[0])
+            action = np.argmax(model.predict(prev_obs.reshape(-1, len(prev_obs), 1))[0])
 
         choices.append(action)
         new_observation, reward, done, truncated, info = env.step(action)
